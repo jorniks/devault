7c48a79a7e48197511ad1a4fb99564089807ac21
jorniks <dictforum@gmail.com>
2024-12-15 05:28:23 +0100

    Update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -25,12 +25,10 @@ const NavBar = () => {
   const location = usePathname();
   
   return (
-    <div className="grid grid-cols-12 max-w-7xl mx-auto items-center bg-[#5e5e5e] bg-opacity-50 rounded-2xl py-4 px-6 mt-3 text-white z-10 sticky top-4">
-      <div className="col-span-6 md:col-span-4 flex justify-start">
-        <Image src="/img/logo.png" alt="Databank" className="h-8 w-auto" height={10000} width={10000} />
-      </div>
+    <div className="container flex items-center justify-between bg-[#5e5e5e]/50 rounded-2xl py-4 px-6 z-10 sticky top-4">
+      <Image src="/img/logo.png" alt="Databank" className="h-8 w-auto" height={10000} width={10000} />
 
-      <div className="hidden md:col-span-4 md:flex justify-center md:space-x-2 lg:space-x-6 font-semibold text-lg">
+      <div className="space-x-2 lg:space-x-6 font-semibold text-lg">
         {NavbarLinks.map((link, index) => (
           <Link key={index} href={`${link.href}`} className={`hover:text-[#2B9DDA] transition-colors tracking-wide duration-300 ${location === link.href && "text-[#2B9DDA]"}`}>
             {link.name}
@@ -38,10 +36,7 @@ const NavBar = () => {
         ))}
       </div>
 
-
-      <div className="col-span-6 md:col-span-4 flex justify-end">
-        <WalletButton />
-      </div>
+      <WalletButton />
     </div>
   )
 }
