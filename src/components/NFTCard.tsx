3a0c06178dc926295e00b64d0770df60d676c7b1
Shagbaor Agber(Pyman) <dxtlive@gmail.com>
2024-12-15 18:34:05 +0100

    added different file view format

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -7,7 +7,7 @@ import { useReadContract } from '@/hooks/read/useReadContract'
 
 const NFTCard = ({ nftId }: { nftId: number }) => {
   const [fullscreenNftUrl, setFullscreenNftUrl] = useState("");
-  const [NFTInfo, setNFTInfo] = useState<NFTMetadata>(Object)
+  const [NFTInfo, setNFTInfo] = useState<NFTMetadata>({})
   const { getNFTMetadata } = useReadContract()
 
   const handleNftDoubleClick = (nftUrl: string) => {
@@ -24,16 +24,30 @@ const NFTCard = ({ nftId }: { nftId: number }) => {
     getNFTMetadata(nftId).then(setNFTInfo)
   }, [getNFTMetadata, nftId])
   
-  
+  const imageTypes = ["png", "jpeg", "jpg", 'gif'];
+
   return (
     <div className="">
       <div onDoubleClick={() => handleNftDoubleClick(NFTInfo?.documentURI)}
-        className="flex flex-col gap-1 py-3 px-2 bg-[#2B9DDA]/40 rounded-lg cursor-pointer hover:-translate-y-1 transition-all duration-300"
+        className="flex flex-col gap-1 p-3 bg-[#1E1E1E] rounded-md cursor-pointer border border-[#2B9DDA] hover:bg-[#3f3f3f] hover:border-gray-900 transition-colors h-11/12"
       >
-        <Image src={NFTInfo?.documentURI} alt={NFTInfo?.documentName} className="rounded-lg object-contain h-20 md:h-40 w-full" height={10000} width={10000} />
+        {NFTInfo.documentType === `${imageTypes[0]}` || NFTInfo.documentType === `${imageTypes[1]}` || NFTInfo.documentType === `${imageTypes[2]}` ? (
+          <Image src={NFTInfo?.documentURI} alt={NFTInfo?.documentName} className="rounded-lg w-full h-20 md:h-40" height={10000} width={10000} />
+        ) : (
+          <iframe 
+            src={NFTInfo?.documentURI} 
+            title={NFTInfo?.documentName} 
+            className="rounded-lg w-full h-20 md:h-40" 
+            height={10000} 
+            width={10000} 
+            frameBorder="0" 
+            allowFullScreen
+          ></iframe>
+        )}
+
 
         <div className="text-start mt-2 ml-2 mb-2 ">
-          <h6 className="">{NFTInfo?.documentName}</h6>
+          <h6 className="text-[#2B9DDA]">{NFTInfo?.documentName}</h6>
           <h6 className="md:text-xl md:mb-2">{NFTInfo?.documentType}</h6>
         </div>
 
@@ -45,7 +59,20 @@ const NFTCard = ({ nftId }: { nftId: number }) => {
       {fullscreenNftUrl && (
         <div onClick={handleCloseFullscreen} className="fixed inset-0 w-full h-full bg-[#5e5e5e] bg-opacity-50 flex items-center justify-center z-50 transition-opacity duration-300 ease-in-out">
           <div onClick={(e) => e.stopPropagation()} className="relative max-w-4xl w-full mx-4 bg-[#1E1E1E] rounded-lg overflow-hidden shadow-2xl">
-            <Image src={fullscreenNftUrl} alt="Fullscreen NFT" className="w-full h-auto object-contain max-h-[80vh]" height={10000} width={10000} />
+          {NFTInfo.documentType === `${imageTypes[0]}` || NFTInfo.documentType === `${imageTypes[1]}` || NFTInfo.documentType === `${imageTypes[2]}` ? (
+          <Image src={fullscreenNftUrl} alt="Fullscreen NFT" className="w-full h-auto object-contain max-h-[80vh]" height={10000} width={10000} />
+        ) : (
+          <iframe 
+            src={NFTInfo?.documentURI} 
+            title={NFTInfo?.documentName} 
+            className="w-full h-full object-contain max-h-[80vh]" 
+            height={10000} 
+            width={10000} 
+            frameBorder="1" 
+            allowFullScreen
+          ></iframe>
+        )}
+
 
             <Button onClick={handleCloseFullscreen} aria-label="Close fullscreen view" className="absolute top-4 right-4 text-white hover:text-gray-300 transition-colors duration-200 ease-in-out">
               <X size={24} />
